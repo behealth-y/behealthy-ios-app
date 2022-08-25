29e5a29b10e6118c326a0369acbc30db8478c2dd
gusdn5387 <gusdn5387@naver.com>
2022-08-25 11:39:55 +0900

    디렉토리 구조 설정 및 LoginViewController 추가

--- Be-Healthy/ViewController/Authentication/LoginViewController.swift
+++ Be-Healthy/ViewController/Authentication/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Be-Healthy
 //
 //  Created by 박현우 on 2022/08/25.
@@ -7,14 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemYellow
     }
-
-
 }
-
---
+++ Be-Healthy/ViewController/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Be-Healthy
+//
+//  Created by 박현우 on 2022/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
+
