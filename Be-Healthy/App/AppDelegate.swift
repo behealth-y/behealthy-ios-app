2bbc3abf82c4b5b0906b71f8da542a29b91aa803
gusdn5387 <gusdn5387@naver.com>
2022-08-25 14:32:52 +0900

    LaunchScreen (스플래시) 노출 시간 조정

--- Be-Healthy/App/AppDelegate.swift
+++ Be-Healthy/App/AppDelegate.swift
@@ -13,7 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // LaunchScreen 노출 시간 조정
+        Thread.sleep(forTimeInterval: 1.0)
+        
         return true
     }
 
