2ddb3775f0918f884548bd95f89700682babd9ef
gusdn5387 <gusdn5387@naver.com>
2022-09-21 17:23:54 +0900

    홈 화면 > setupNavigationBar, setupLayout 추가

--- Be-Healthy/ViewController/HomeViewController.swift
+++ Be-Healthy/ViewController/HomeViewController.swift
@@ -6,12 +6,65 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
 class HomeViewController: UIViewController {
-
+    /// scrollView 변수 초기화
+    lazy var scrollView = UIScrollView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .white
+        setupNavigationBar()
+        setupLayout()
     }
 }
+
+// MARK: - 레이아웃 설정 관련
+extension HomeViewController {
+    /// 네비게이션 바 설정
+    fileprivate func setupNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+//        appearance.shadowColor = .clear
+        appearance.backgroundColor = .white
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.init(named: "mainColor")!]
+        
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationController?.navigationBar.standardAppearance = appearance
+        
+        navigationController?.navigationBar.tintColor = UIColor.init(named: "mainColor")
+        
+        navigationItem.title = "BE HEALTHY"
+    }
+    
+    /// 레이아웃 설정
+    fileprivate func setupLayout() {
+        view.backgroundColor = .systemPink
+    }
+}
+
+#if DEBUG
+
+import SwiftUI
+
+// OPTION + CMD + ENTER: 미리보기 화면 띄우기, OPTION + CMD + P: 미리보기 resume
+struct HomeViewControllerPresentable: UIViewControllerRepresentable {
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        HomeViewController()
+    }
+}
+
+struct HomeViewControllerPresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        HomeViewControllerPresentable()
+    }
+}
+
+#endif
+
+
+
